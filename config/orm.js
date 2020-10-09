be04076bd49365143998d1b6a8d53d2c5e58931c
JenIglar <jeniglar@gmail.com>
2020-10-09 18:54:46 -0400

    Fixed orm

--- config/orm.js
+++ config/orm.js
@@ -70,6 +70,14 @@ var orm = {
       cb(result);
     });
   },
-}
+  remove: function(table, col, val, cb) {
+    connection.query("DELETE FROM ?? WHERE ?? = ?", [table, col, val], function(err, data) {
+      if(err)
+        throw err;
+      console.log(data);
+      return cb(data);
+    });
+  }
+};
   
 module.exports = orm;
